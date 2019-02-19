ca5f11c586aa69d7cdb78be432ef99c375aa6f20
Ivan Otnoshennyi <dominus3339@gmail.com>
2019-02-19 15:48:43 +0200

    fix

--- src/components/Card/CardText.js
+++ src/components/Card/CardText.js
@@ -2,9 +2,13 @@ import React from "react";
 import styled from "styled-components";
 // import NanoClamp from "nanoclamp";
 
-const Clamp = ({ children, className, lines }) => (
-  <div className={className}>{children.substring(0, lines * 10) + '...'}</div>
-);
+const Clamp = ({ children, className, lines }) => {
+  if (!children) return null;
+
+  return (
+    <div className={className}>{children.substring(0, lines * 10) + "..."}</div>
+  );
+};
 
 const CardText = styled(Clamp)`
   &&& {
